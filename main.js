ea96538e771c286c834e693a8d466c766bb28270
hanaheyo <violets5618@gmail.com>
2020-08-18 15:15:09 +0900

    Navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar__toggle-btn');
+const menu = document.querySelector('.navbar__menu');
+const contacts = document.querySelector('.navbar__contacts')
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    contacts.classList.toggle('active');
+});
